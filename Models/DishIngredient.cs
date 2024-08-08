227403db3d8b4b92acf791424f8bd0e5ace139b7
maimaliha21 <maimaliha8@gmail.com>
2024-08-09 01:17:54 +0300

    second commit saved

--- Models/DishIngredient.cs
+++ Models/DishIngredient.cs
@@ -2,5 +2,11 @@
 {
     public class DishIngredient
     {
+        public int DishId { get; set; }
+        public Dish Dish { get; set; }
+
+        public int IngredientId { get; set; }
+        public Ingredient Ingredient { get; set; }
+
     }
 }
--- obj/Debug/net8.0/Menu.AssemblyInfo.cs
+++ obj/Debug/net8.0/Menu.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Menu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+431102878b9c3cba67e818ceb0004c03be274c83")]
 [assembly: System.Reflection.AssemblyProductAttribute("Menu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Menu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
